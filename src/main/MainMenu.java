d32c6aa1ebfb7d9c034b7dd26ba05724848e47d2
Reilly Freret <freret@wustl.edu>
2022-03-28 14:47:53 -0500

    dependencies, buildpath updates

---
+++ src/main/MainMenu.java
@@ -0,0 +1,14 @@
+package main;
+
+public class MainMenu {
+	
+	public static int asdf() {
+		return 1;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		asdf();
+	}
+
+}

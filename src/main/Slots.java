a3b070091beb52e979e0e6747ef79696a98cd49b
laurabucchieri <laurabucchieri@wustl.edu>
2022-03-30 10:01:45 -0500

    code and tests for slots

--- src/main/Slots.java
+++ src/main/Slots.java
@@ -1,10 +1,98 @@
 package main;
 
+import java.util.Scanner;
+
 public class Slots {
+	
+	private int credits;
+	private int numPlays;
+	private int numWins;
+	
+	public Slots() {
+		credits = 50;
+		numPlays = 0;
+		numWins = 0;
+	}
+	
+	public int getCredits() {
+		return credits;
+	}
+
+	public int getNumPlays() {
+		return numPlays;
+	}
+
+	public int getNumWins() {
+		return numWins;
+	}
+
+	public void initiate() {
+		Scanner scan = new Scanner(System.in);
+		int input = 0;
+		while (input >= 0) {
+			System.out.println("Plays: " + numPlays + " Wins: " + numWins + " Credits: " + credits);
+			System.out.println("Play slots? (enter number of times to play, -1 for quit)");
+		    input = scan.nextInt();
+		    for(int i=0; i<input; i++) {
+		    	this.play();
+		    }
+		}
+	    System.out.println("Bye");
+	    scan.close();
+	}
+	
+	public void play() {
+		if(this.hasEnoughCredits()) {
+			credits--;
+			numPlays++;
+			int[] slotsNumbers = this.generateNumbers();
+			this.printNumbers(slotsNumbers);
+			if(checkIfWin(slotsNumbers)) {
+				credits += 10;
+				numWins++;
+				System.out.println("Congrats! You win!\n");
+			}
+			else {
+				System.out.println("You lose :(\n");
+			}
+		}
+		else {
+			System.out.println("You don't have enough money :(");
+		}
+	}
+	
+	public boolean hasEnoughCredits() {
+		return (this.credits > 0);
+	}
+	
+	public int[] generateNumbers() {
+		int[] slotsNumbers = new int[3];
+		for(int i=0; i<3; i++) {
+			slotsNumbers[i] = (int)(Math.random() * 5);
+		}
+		return slotsNumbers;
+	}
+	
+	public void printNumbers(int[] slotsNumbers) {
+		for(int i=0; i<slotsNumbers.length; i++) {
+			System.out.print(slotsNumbers[i] + "... ");
+			try {
+				Thread.sleep(500);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	public static boolean checkIfWin(int[] slotsNumbers) {
+		return (slotsNumbers[0] == slotsNumbers[1] && slotsNumbers[1] == slotsNumbers[2]);
+	}
+	
+	
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Slots slots = new Slots();
+		slots.initiate();
 	}
 
 }
--- src/test/SlotsTest.java
+++ src/test/SlotsTest.java
@@ -4,11 +4,67 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+import main.Slots;
+
 class SlotsTest {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void testConstructor() {
+		Slots slots = new Slots();
+		
+		int credits = slots.getCredits();
+		int plays = slots.getNumPlays();
+		int wins = slots.getNumWins();
+		
+		assertEquals(50, credits);
+		assertEquals(0, plays);
+		assertEquals(0, wins);
+	}
+	
+	@Test
+	void testHasEnoughCredits() {
+		Slots slots = new Slots();
+		
+		boolean enoughCredits = slots.hasEnoughCredits();
+		
+		assertEquals(true, enoughCredits);
+	}
+	
+	@Test
+	void testCheckIfWin() {
+		int[] winningHand = {4, 4, 4};
+		int[] losingHand = {1, 2, 2};
+		
+		boolean areEqual = Slots.checkIfWin(winningHand);
+		boolean arentEqual = Slots.checkIfWin(losingHand);
+		
+		assertEquals(true, areEqual);
+		assertEquals(false, arentEqual);
+	}
+	
+	@Test
+	void testNumCreditsAfterGameplay() {
+		Slots slots = new Slots();
+		for (int i=0; i<10; i++) {
+			slots.play();
+		}
+		int wins = slots.getNumWins();
+		int credits = slots.getCredits();
+		int expectedCredits = 50 - 10 + (wins * 10);
+		
+		assertEquals(expectedCredits, credits);
 	}
 
 }
+
+/*
+ * 1. initiate play
+ * 	a. user input initiating play
+ * 	b. check enough credits
+ * 		i. if so, subtract one, add one to plays and play
+ * 		ii. if not, print message
+ * 2. generate 3 random numbers and print them
+ * 3. check if these 3 are equal
+ * 	a. if so: add points (10?), print message
+ * 	b. if not, print message
+ * */

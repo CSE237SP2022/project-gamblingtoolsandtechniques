36770c0e4d183f5db0edf9bf28985bb950370a3f
Reilly Freret <freret@wustl.edu>
2022-04-11 13:18:54 -0500

    save state

---
+++ src/main/War.java
@@ -0,0 +1,28 @@
+package main;
+
+public class War {
+	
+	private Deck humanCards;
+	private Deck cpuCards;
+	
+	/**
+	 * Returns new War object
+	 * @param numDecks specifies the number of decks to distribute among players
+	 */
+	public War(int numDecks) {
+		Deck d = new Deck(numDecks);
+		// TODO: less-janky means of dealing cards? Might have to update the Deck class
+		boolean turn = false;
+		while (d.getSize() >= 0) {
+			if (turn) humanCards.add(d.deal()); else cpuCards.add(d.deal());;
+			turn = !turn;
+		}
+	}
+	
+	public War() {
+		this(1);
+	}
+	
+	
+
+}

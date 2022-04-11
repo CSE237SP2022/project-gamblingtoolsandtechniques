b63f742c3783b53eded609e0c935a329e83fc5c1
kaitlinday <54457484+kaitlinday@users.noreply.github.com>
2022-04-11 13:47:00 -0500

    add BlackJackTest

---
+++ src/test/BlackJackTest.java
@@ -0,0 +1,15 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class BlackJackTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+	
+
+}
